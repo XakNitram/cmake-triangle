70adfc959239ff5a9cefbce7df2b215f74f3a00a
Tate Mioton <tatemioton@outlook.com>
2020-12-03 18:29:41 -0600

    Split Model and Color Data

--- src/Triangle.cpp
+++ src/Triangle.cpp
@@ -3,22 +3,31 @@
 
 
 Triangle::Triangle(float x, float y, float width, float height) {
-    std::array<float, 15> modelData {
-        x,               y,          1.0f, 0.0f, 0.0f,
-        x + width,       y,          0.0f, 1.0f, 0.0f,
-        x + (width / 2), y + height, 0.0f, 0.0f, 1.0f
+    std::array<float, 6> modelData {
+        x,               y,
+        x + width,       y,
+        x + (width / 2), y + height,
+    };
+
+    std::array<float, 9> colorData {
+        1.0f, 0.0f, 0.0f,
+        0.0f, 1.0f, 0.0f,
+        0.0f, 0.0f, 1.0f
     };
 
     m_array.bind();
     m_model.usage(lwvl::Usage::Static);
     m_model.construct(modelData.begin(), modelData.end());
 
-    m_model.bind();
-    m_array.attribute(2, GL_FLOAT, 5 * sizeof(float), 0);
-    m_array.attribute(3, GL_FLOAT, 5 * sizeof(float), 2 * sizeof(float));
+    m_color.usage(lwvl::Usage::Static);
+    m_color.construct(colorData.begin(), colorData.end());
 
+    m_model.bind();
+    m_array.attribute(2, GL_FLOAT, 2 * sizeof(float), 0);
+
+    m_color.bind();
+    m_array.attribute(3, GL_FLOAT, 3 * sizeof(float), 0);
     m_array.clear();
-    m_model.clear();
 }
 
 void Triangle::draw() {
--- src/Triangle.h
+++ src/Triangle.h
@@ -1,3 +1,4 @@
+#pragma once
 #include "pch.h"
 #include "Renderer/Buffer.h"
 #include "Renderer/VertexArray.h"
@@ -6,6 +7,7 @@
 class Triangle {
     lwvl::VertexArray m_array;
     lwvl::ArrayBuffer m_model;
+    lwvl::ArrayBuffer m_color;
 
 public:
     Triangle(float x, float y, float width, float height);
